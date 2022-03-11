1ad2341ea3de000e0deb1aa0ebb6e5f3cab03073
Rihem Farjallah <rihemfarjallah4@gmail.com>
2022-03-11 15:06:21 +0100

    first commit

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const header = () => {
+    return ( 
+        <div className="header">
+            
+        </div>
+     );
+}
+ 
+export default header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDu5BsLZi5cveul3Z3qFPaOyqUxCjFrobc",
+    authDomain: "challenge-bdc8f.firebaseapp.com",
+    projectId: "challenge-bdc8f",
+    storageBucket: "challenge-bdc8f.appspot.com",
+    messagingSenderId: "1053325341570",
+    appId: "1:1053325341570:web:e96341e271f638b231959b",
+    measurementId: "G-BVR9G799WM"
+  };
