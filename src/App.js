37f1b3de3da0ab44005df68c88a82866e2c30b06
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2022-03-12 18:57:15 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import "./App.css";
 import Home from "./Home";
+import Checkout from "./Checkout";
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
         <Routes>
+          <Route path="/checkout" element={<Checkout />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -1,36 +1,45 @@
-import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-const header = () => {
-    return ( 
-        <div className="header">
-          <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="logo" className="header__logo" />  
-
-          <div className="header__search">
-            <input type="text" className="header__searchInput" />
-            <SearchIcon className='header__searchIcon' />
-
-          </div>
-          <div className="header__nav">
-              <div className="header__option">
-                <span className="header__OptionLineOne"> Hello Guest</span>
-                <span className="header__OptionLineTwo">Sign In</span>
-              </div>
-              <div className="header__option">
-              <span className="header__OptionLineOne"> Returns</span>
-                <span className="header__OptionLineTwo">& Orders</span>  
-              </div>
-              <div className="header__option">
-                <span className="header__OptionLineOne">Your</span>
-                <span className="header__OptionLineTwo">Prime</span> 
-              </div>
-              <div className="header__optionBasket">
-                <ShoppingBasketIcon />
-                <span className="header__optionLineTwo header__basketCount">0</span>
-              </div>
-          </div>
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+const Header = () => {
+  const [{basket}, dispatch] = useStateValue();
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="logo"
+          className="header__logo"
+        />
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__OptionLineOne"> Hello Guest</span>
+          <span className="header__OptionLineTwo">Sign In</span>
         </div>
-     );
-}
- 
-export default header;
+        <div className="header__option">
+          <span className="header__OptionLineOne"> Returns</span>
+          <span className="header__OptionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__OptionLineOne">Your</span>
+          <span className="header__OptionLineTwo">Prime</span>
+        </div>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
