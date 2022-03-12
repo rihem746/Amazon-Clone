56db10b1d2c03f8a9f51c46b91c63a8341ab8710
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2022-03-12 14:58:35 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Header from './Header';
-import './App.css';
-import Home from './Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import "./App.css";
+import Home from "./Home";
 function App() {
   return (
-    <div className="App">
-     <Header />
-     <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
