dfabf89460c1df0ca911c476404cc9440a489208
Rebai Souha <souharebai111@gmail.com>
2022-03-16 19:47:45 +0100

    third commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,13 +5,14 @@ import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal'
 
 function Checkout() {
-    const [{basket}, dispath] = useStateValue();
+    const [{basket , user}, dispath] = useStateValue();
 
   return (
     <div className='checkout'>
         <div className="checkout__left">
             <img className='checkout__ad' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
             <div>
+                <h3>Hello, {user?.email}</h3>
                 <h2 className="checkout__title">
                     Your shopping Basket
                 </h2>
