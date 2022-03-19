d4a3686be8ef78d97a05b4c2168a0b27dc1f2aa2
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2022-03-19 22:25:12 +0100

    payment v1.9.1

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
-import axios from "axios";
+import axios from "./axios";
 
 const instance= axios.create({
-    baseURL: 'http://localhost:5001/challenge-bdc8f/us-central1/api' //THE API (CLOUD FUNCTION) url
+    baseURL: 'http://localhost:5001/challenge-bdc8f/us-central1/api' 
+    //THE API (CLOUD FUNCTION) url
 });
 export default instance;
